3fd5e825798536147d8bcb2ea7a3404ae49cb333
Martin Nussbaum <maxvomrudel@gmail.com>
2022-12-14 15:58:45 +0100

    inhalt von overview

--- test4.py
+++ test4.py
@@ -7,26 +7,65 @@ import plotly.express as px
 import pickle
 from dash import Dash, dash_table, dcc, html
 import pandas as pd
-from dash import html
-import dash_html_components as html
+
+
+
+
+
+
+
+with open("data/metrics_summary.pickle", 'rb') as handle:
+    testdatei= pickle.load(handle)
+
+
+werte= testdatei.groupby(["BfxProjekt"]).mean(numeric_only=True).apply(round)
+
+fig1 = px.line(werte, x=werte.index , y = "Estimated Number of Cells")
+fig2 = px.line(werte, x=werte.index , y = "Mean Reads per Cell")
+fig3 = px.line(werte, x=werte.index , y = "Median Genes per Cell")
+fig4 = px.line(werte, x=werte.index , y = "Median Genes per Cell")
+
+def wertebekommen(input):
+    #testdatei.groupby("samplename").apply(Summe += testdatei[input])
+    Summe = sum(testdatei[input])
+    return Summe
+
+
+
+
+Numberofcells = wertebekommen("Estimated Number of Cells")
+
+
+Tabelleninhalt =[werte.size, testdatei.size, Numberofcells]
+Index = ["1. Number of experiments", "2. Number of samples", "3. Total number of cells"]
+
 
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.layout = dbc.Container(
     [dbc.Row(
             [
-                dbc.Col(html.Div(style= {"border":"double"}, children=[html.H1("overview")]), width=3),
+                dbc.Col(html.Div(style= {"border":"double"}, children=[html.H1("overview"),
+                
+                
+                
+                
+                
+                #dash_table.DataTable(data={"name": Index, "id": Tabelleninhalt})
+                ]), width=3),
 
                 dbc.Col(html.Div(children=[
                     html.Div(style= {"border":"double"}, children=[
                          dbc.Row([
             
                 dbc.Col(style={"border":"double",'textAlign':'center'},children=[
-                        html.P("Cells"),
-                        ]),
+                        html.P("Estimated number of cells"),
+                        dcc.Graph(id="fig1",figure=fig1, style={})
+                        ], width=6),
 
                 dbc.Col(style={"border":"double",'textAlign':'center'},children=[
-                        html.P("reads in cells")
-                    ])
+                        html.P("Mean reads in cells"),
+                        dcc.Graph(id="fig2",figure=fig2, style={})
+                    ], width=6)
 
                     ])]),
 
@@ -35,12 +74,14 @@ app.layout = dbc.Container(
                         dbc.Row([
             
                 dbc.Col(style={"border":"double",'textAlign':'center'},children=[
-                        html.P("number of genes"),
-                        ]),
+                        html.P("Estimated number of genes"),
+                        dcc.Graph(id="fig3",figure=fig3, style={})
+                        ], width=6),
 
                 dbc.Col(style={"border":"double",'textAlign':'center'},children=[
-                        html.P("number of samples")
-                    ])
+                        html.P("Number of samples"),
+                        dcc.Graph(id="fig4",figure=fig4, style={})
+                    ], width=6)
 
                     ])
                     ]),
@@ -54,47 +95,7 @@ app.run_server(debug=True)
 
 
 
-
-
-
-content = html.Div(
-    [
-        dbc.Row(
-            [
-                dbc.Col(
-                    [
-                        html.P('Distribution of Categorical Variable'),
-                        ]),
-                dbc.Col(
-                    [
-                        html.P('Distribution of Continuous Variable')
-                    ])
-            ],
-            style={"height": "50vh"}),
-        dbc.Row(
-            [
-                dbc.Col(
-                    [
-                        html.P('Correlation Matrix Heatmap')
-                    ])
-            ],
-            style={"height": "50vh"}
-            )
-        ]
-    )
-
-app.layout = dbc.Container(
-    [
-        dbc.Row(
-            [
-                dbc.Col(sidebar, width=3, className='bg-light'),
-                dbc.Col(content, width=9)
-                ]
-            ),
-        ],
-    fluid=True
-    )
-
-
 if __name__ == "__main__":
     app.run_server(debug=True, port=1234)
+
+
