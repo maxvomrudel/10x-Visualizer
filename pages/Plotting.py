e00f578cb9917981e5b23e37ce330a87db35f01e
Martin Nussbaum <maxvomrudel@gmail.com>
2023-05-16 14:20:57 +0200

    c c

--- pages/Plotting.py
+++ pages/Plotting.py
@@ -17,15 +17,33 @@ dash.register_page(__name__)
 with open("data/metrics_summary.pickle", 'rb') as handle:
     source = pickle.load(handle)
 
-values = source.groupby(["BfxProjekt"]).mean().apply(round)
-numeric_columns = values.select_dtypes(include="number").columns
-types = ["line","bar","scatter"]
+# get all, numeric and non-numeric columns
 every_column = source.columns
+numeric_columns = source.select_dtypes(include="number").columns
 not_numeric_columns = source.select_dtypes(exclude="number").columns.tolist()
+
+# aggregate by BfxProjekt
+def mean_and_round(x):
+    return round(mean(x))
+
+def first(x):
+    return x[0]
+
+agg_funkt = {}
+for c in numeric_columns:
+    agg_funkt[c] = 'mean'
+for c in not_numeric_columns:
+    agg_funkt[c] = 'first'
+    
+values = source.groupby(["BfxProjekt"], as_index=False).agg(agg_funkt)
+
+# filter not_numeric_columns to what is allowed
 not_numeric_columns.append("-")
 not_numeric_columns.remove("SampleName")
 not_numeric_columns.remove("Key")
 not_numeric_columns.remove("Filename")
+
+types = ["line","bar","scatter"]
 
 fig = px.line(values, x=values.index, y=numeric_columns[0])
 
@@ -121,15 +139,30 @@ row = html.Div(children=[
     ],
 )
 
-def make_graph(y, type_of_diagram,x,z,f):
+def make_graph(y, type_of_diagram,x,z,f=None):
     if f == "-": f = None
     if type_of_diagram=="line":
-        return px.line(values, x=values.index, y=y, facet_col=f)
+        fig = px.line(values, x=values.index, y=y, facet_col=f, facet_col_wrap=2)
+        fig.update_xaxes(matches=None)
+        fig.for_each_xaxis(lambda xaxis: xaxis.update(showticklabels=True))
+        fig.update_yaxes(matches=None)
+        fig.for_each_yaxis(lambda yaxis: yaxis.update(showticklabels=True))
+        return fig
     elif type_of_diagram=="scatter":
-        return px.scatter(source, x=x, y=y, color=z, facet_col=f)
+        fig = px.scatter(source, x=x, y=y, color=z, facet_col=f, facet_col_wrap=3)
+        fig.update_xaxes(matches=None)
+        fig.for_each_xaxis(lambda xaxis: xaxis.update(showticklabels=True))
+        fig.update_yaxes(matches=None)
+        fig.for_each_yaxis(lambda yaxis: yaxis.update(showticklabels=True))
+        return fig
     else:
-        return px.bar(values, x=values.index, y=y, facet_col=f)
-
+        fig = px.bar(values, x=values.index, y=y, facet_col=f, facet_col_wrap=2)
+        fig.update_xaxes(matches=None)
+        fig.for_each_xaxis(lambda xaxis: xaxis.update(showticklabels=True))
+        fig.update_yaxes(matches=None)
+        fig.for_each_yaxis(lambda yaxis: yaxis.update(showticklabels=True))
+        return fig
+    
 diagram = dcc.Graph(id="output_diagram", figure=fig, style={'height': "85vh", "width":"170vh",'textAlign': 'center' })
 
 content = dbc.Container(
